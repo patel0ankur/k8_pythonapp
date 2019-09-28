061dcb47de0856f07aee33fde1812f87661af7f3
Ankur <patel0ankur@gmail.com>
2019-09-28 13:39:04 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  target = os.environ.get('TARGET', 'World')
-return 'Hello {}!\n'.format(target)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == "__main__":
-  app.run(debug = True, host = '0.0.0.0', port = int(os.environ.get('PORT', 8080)))
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '0.0.0.0')
